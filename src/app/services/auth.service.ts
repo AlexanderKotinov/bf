48a52e193efaf85cf508e8998e2fe2486bb7811b
AlexanderKotinov <alexander777ck@gmail.com>
2018-10-10 23:01:01 +0300

    Added user model.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from '../models/user.model';
+import { Observable } from 'rxjs/index';
+import * as firebase from 'firebase/app';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private _user: any;
+  private _user: Observable<firebase.User>;
 
   constructor(private _auth: AngularFireAuth) { }
 
-  registerUser(authData: any): void {
+  registerUser(authData: User): void {
     // this._uiService.loadingStateChanged.next(true);
     this._auth.auth.createUserWithEmailAndPassword(authData.email, authData.password)
       .then(res => {
         // this._uiService.loadingStateChanged.next(false);
-        this._user = this._auth.authState;
         // this._router.navigate(['/training']);
       })
       .catch(error => {
